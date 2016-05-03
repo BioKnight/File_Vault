ea8d4196419c28ae3915c25192f6f15461b2ffca
Randy <Randy@RANDY-PC>
2016-05-03 07:58:09 -0400

    Creation

---
+++ File_Vault/File_Vault/Form1.Designer.vb
@@ -0,0 +1,48 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm_Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.tree_Main = New System.Windows.Forms.TreeView()
+        Me.SuspendLayout()
+        '
+        'tree_Main
+        '
+        Me.tree_Main.Location = New System.Drawing.Point(0, 0)
+        Me.tree_Main.Name = "tree_Main"
+        Me.tree_Main.Size = New System.Drawing.Size(121, 628)
+        Me.tree_Main.TabIndex = 0
+        '
+        'frm_Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(500, 628)
+        Me.Controls.Add(Me.tree_Main)
+        Me.Name = "frm_Main"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents tree_Main As TreeView
+End Class
---
+++ File_Vault/File_Vault/Form1.vb
@@ -0,0 +1,33 @@
+﻿Imports System
+Imports System.IO
+Imports System.Security
+Imports System.Security.Cryptography
+Imports System.Runtime.InteropServices
+Imports System.Text
+
+' Tutorial: https://support.microsoft.com/en-us/kb/301070
+' Tutorial: http://stackoverflow.com/questions/2301997/how-to-encrypt-a-file-folder-using-vb-net
+
+Public Class frm_main
+    Private application_Path As DirectoryInfo = New DirectoryInfo(".\")
+
+    Private Sub frm_main_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ' Read the current directory structure
+
+        'MsgBox(application_Path.FullName)
+        For Each folder As DirectoryInfo In application_Path.GetDirectories
+            'MsgBox(folder.FullName)
+            tree_Main.Nodes(0).Nodes.Add(create_Node(folder))
+        Next
+    End Sub
+
+    Private Function create_Node(ByRef folder As DirectoryInfo) As TreeNode
+        Dim tempnode As New TreeNode(folder.Name)
+        For Each child_folder As DirectoryInfo In folder.GetDirectories
+            tempnode.Nodes.Add(create_Node(child_folder))
+        Next
+
+        Return tempnode
+    End Function
+
+End Class
