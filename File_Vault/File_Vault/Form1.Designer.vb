da28aef076e90b1510e3fec3a26eb4c52b092407
Randy <Randy@RANDY-DELL3010>
2016-05-04 16:11:11 -0400

    start

--- File_Vault/File_Vault/Form1.Designer.vb
+++ File_Vault/File_Vault/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class frm_Main
+Partial Class frm_main
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
