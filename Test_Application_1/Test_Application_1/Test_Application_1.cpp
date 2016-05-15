84827eeaeb6ea85b3d227b6ce6cd8db07172422a
Randy <Randy@RANDY-PC>
2016-05-15 09:45:03 -0400

    Complete, including comments.

--- Test_Application_1/Test_Application_1/Test_Application_1.cpp
+++ Test_Application_1/Test_Application_1/Test_Application_1.cpp
@@ -1,11 +1,18 @@
 // Test_Application_1.cpp : main project file.
 
 #include "stdafx.h"
+#include <iostream>
+#include <string>
 
 using namespace System;
 
-int main(array<System::String ^> ^args)
+int main(array<System::String ^> ^args)		// Main function, the array is any command line arguments passed to the application
+											// This will also work simple as main(),
+											// Shown is the default Microsoft way, I use a standard C++ alteration.
 {
-    Console::WriteLine(L"Hello World");
+	printf("Hello World\n\n");				// Standard C++ Version
+    Console::WriteLine(L"Hello World");		// Microsoft Version
+	std::string input;						// String to contain our input
+	std::getline(std::cin, input);			// C++ getline, using C cin as its stream, this is just to keep the dialog from closing.
     return 0;
 }
