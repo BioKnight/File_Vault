4ba949eceb2b26f936b3d48639db987634d5555c
Randy <Randy@RANDY-PC>
2016-05-15 21:28:51 -0400

    Started implimenting the Cryptography class.

---
+++ File_Vault/File_Vault/Encryption.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Security.Cryptography
+
+
+Public Class Encryption
+
+    ' Private section
+    Private Const encrypton_Key As String = "Linux_Samba_5285250_8902_7798_BioKnight_1981"
+    Private preEncrypt_Object As Object
+    Private encrypted_Object As Object
+
+    Private Sub encrypt_Object()
+
+    End Sub
+
+    ' Public section
+
+    Sub New(ByVal obj_Encrypt As Object)
+        preEncrypt_Object = obj_Encrypt
+
+    End Sub
+
+    Public Function get_Encrypted_Object() As Object
+
+    End Function
+
+
+End Class
