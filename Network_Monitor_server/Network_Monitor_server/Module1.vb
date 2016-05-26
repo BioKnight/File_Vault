00b5e5556489a711da66151beeb19a62db441928
Randy <Randy@RANDY-PC>
2016-05-25 22:01:48 -0400

    broadcast system functional, far from dynamic.

--- Network_Monitor_server/Network_Monitor_server/Module1.vb
+++ Network_Monitor_server/Network_Monitor_server/Module1.vb
@@ -1,55 +1,87 @@
 ﻿
 Imports System.Net
-Imports System.Net.NetworkInformation
 Imports System.Net.Sockets
 Imports System.Text
+Imports System.Threading
 
 Module Module1
 
-    Sub Main()
-        Dim host_Name As String = System.Net.Dns.GetHostName()
+    Private str_IP As String = "192.168.3.0"
+    Private ipadd_broadcast_Address As IPAddress = IPAddress.Parse("192.168.3.255")
+    Private str_Name As String = "Default"
+    Private str_Computer_Name As String = "Default_PC"
+    Private str_Uptime As String = "A_While"
+    Private socket As New Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
+    Private end_Point As IPEndPoint
+    Private send_buffer As Byte()
+
+    Sub Main(ByVal args As String())
+        process_CLI_Args(args)
+
+        'Dim host_Name As String = System.Net.Dns.GetHostName()
         ' This will not work in all situations, revamp
-        Dim IP_Address As String = System.Net.Dns.GetHostEntry(host_Name).AddressList(4).ToString()
-        Dim netmask As String
+        Dim IP_Address As String = str_IP
+        'Dim netmask As String
 
 
-        Dim Interfaces As NetworkInterface() = NetworkInterface.GetAllNetworkInterfaces()
-        Dim adapter As NetworkInterface
-        Dim adapter_Properties As IPInterfaceProperties = Nothing
-        Dim myGateways As GatewayIPAddressInformationCollection = Nothing
+        'Dim Interfaces As NetworkInterface() = NetworkInterface.GetAllNetworkInterfaces()
+        'Dim adapter As NetworkInterface
+        'Dim adapter_Properties As IPInterfaceProperties = Nothing
+        'Dim myGateways As GatewayIPAddressInformationCollection = Nothing
 
-        For Each adapter In Interfaces
-            If adapter.NetworkInterfaceType = NetworkInterfaceType.Loopback Then
-                Continue For
-            End If
-            Dim adapter_Name As String = adapter.Name
-            Dim adapter_Description As String = adapter.Description
-            adapter_Properties = adapter.GetIPProperties
-            myGateways = adapter_Properties.GatewayAddresses
-            Dim IPInfo As UnicastIPAddressInformationCollection = adapter.GetIPProperties().UnicastAddresses
-            Dim properties As IPInterfaceProperties = adapter.GetIPProperties()
 
-            For Each IPAddressInfo As UnicastIPAddressInformation In IPInfo
-                If IPAddressInfo.Address.ToString = IP_Address Then
-                    netmask = IPAddressInfo.IPv4Mask.ToString
-                    MsgBox(IP_Address & " " & netmask)
-                End If
-            Next
+        end_Point = New IPEndPoint(ipadd_broadcast_Address, 11000)
 
-            For Each Gateway As GatewayIPAddressInformation In myGateways
-                'TextBox1.AppendText("Gateway IP :" & Gateway.Address.ToString & Environment.NewLine)
-            Next
+        While True
+            Broadcast()
+            Thread.Sleep(10000)
+        End While
 
-            'TextBox1.AppendText("DNS Address :" & properties.DnsAddresses.ToString & Environment.NewLine)
+        socket.Close()
+
+    End Sub
+
+    Private Sub process_CLI_Args(ByVal args As String())
+        If args.Count <= 0 Then Exit Sub
+
+        For counter As Integer = 0 To args.Count - 1
+            Select Case LCase(args(counter))
+                Case "-ip"
+                    str_IP = args(counter + 1)
+                Case "-broadcast"
+                    ipadd_broadcast_Address = IPAddress.Parse(args(counter + 1))
+                Case "-name"
+
+                Case "-uptime"
+
+                Case "-pcname"
+
+
+
+            End Select
         Next
 
 
-
-        Dim socket As New Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
-        Dim broadcast_Address As IPAddress = IPAddress.Parse("192.168.1.255")
-        Dim send_buffer As Byte() = Encoding.ASCII.GetBytes("Message")
-        Dim end_Point As New IPEndPoint(broadcast_Address, 11000)
-        socket.SendTo(send_buffer, end_Point)
     End Sub
 
+    Private Function build_Message() As String
+        Dim message As String = ""
+
+        message &= "Name: " & str_Name
+        message &= " ; "
+        message &= "PC_Name: " & str_Computer_Name
+        message &= " ; "
+        message &= "UpTime: " & str_Uptime
+        message &= " ; "
+        message &= "IP_Addess: " & str_IP
+
+        Return message
+    End Function
+
+    Private Sub Broadcast()
+        Dim message As String = build_Message()
+        send_buffer = Encoding.ASCII.GetBytes(message)
+        Console.WriteLine("Sending: " & message)
+        socket.SendTo(send_buffer, end_Point)
+    End Sub
 End Module
